c80dfba23087ca4a17003e21ed74dd0af7a21bda
jacob berkman <jacob@87k.net>
2015-10-02 18:00:29 -0700

    Initial Commit

---
+++ Balsa/Balsa.h
@@ -0,0 +1,19 @@
+//
+//  Balsa.h
+//  Balsa
+//
+//  Created by jacob berkman on 2015-10-02.
+//  Copyright © 2015 jacob berkman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Balsa.
+FOUNDATION_EXPORT double BalsaVersionNumber;
+
+//! Project version string for Balsa.
+FOUNDATION_EXPORT const unsigned char BalsaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Balsa/PublicHeader.h>
+
+
